71713302479ecd310b39ba2eb9672766c4655453
lcwspr <lcwspr@163.com>
2023-07-01 08:03:30 +0800

    feat: 增加 udp demo

--- p001_go_gateway/001/udp/client/main.go
+++ p001_go_gateway/001/udp/client/main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	// 1 connect to server
+	conn, err := net.DialUDP("udp", nil, &net.UDPAddr{
+		IP:   net.IPv4(127, 0, 0, 1),
+		Port: 9090,
+	})
+
+	if err != nil {
+		fmt.Printf("connect error, err is %v\n", err)
+		return
+	}
+	defer conn.Close()
+
+	for i := 0; i < 100; i++ {
+
+		// 2 send data
+		_, err = conn.Write([]byte("hello server!"))
+		if err != nil {
+			fmt.Printf("send data failed, err: %v\n", err)
+			return
+		}
+
+		// 3 recv data
+		result := make([]byte, 1024)
+		n, remoteAddr, err := conn.ReadFromUDP(result)
+		if err != nil {
+			fmt.Printf("receive data failed, err: %v\n", err)
+			return
+		}
+
+		fmt.Printf("receive from addr: %v data: %v\n", remoteAddr, string(result[:n]))
+
+	}
+
+}
--- p001_go_gateway/001/udp/server/main.go
+++ p001_go_gateway/001/udp/server/main.go
@@ -1,7 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 func main() {
 
 	// step1
+	listen, err := net.ListenUDP("udp", &net.UDPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: 9090,
+	})
+
+	if err != nil {
+		fmt.Printf("listen failed, err : %v\n", err)
+		return
+	}
+
+	// 2 循环读取消息内容
+	for {
+		var data [1024]byte
+		n, addr, err := listen.ReadFromUDP(data[:])
+		if err != nil {
+			fmt.Printf("read failed from add: %v, err : %v\n", addr, err)
+			break
+		}
+
+		go func() {
+
+			// 3 return data
+			fmt.Printf("addr: %v data: %v len: %v\n", addr, string(data[:n]), n)
+			_, err = listen.WriteToUDP([]byte("recv success"), addr)
+			if err != nil {
+				fmt.Printf("write failed, err: %v\n", err)
+			}
+		}()
+	}
 
 }
